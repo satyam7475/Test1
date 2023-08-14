cd6c92186d4111fd8a53adee4b589b8341362e99
Satyam Yadav <satyamlost@gmail.com>
2023-08-14 12:22:26 +0530

    hello

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 
-class HelloWorld {
+class hello {
 	public static void main(String args[])
 	{
-		System.out.println("Hello, World");
+		System.out.println("HelloWorld");
+        System.out.println("My name is Satyam Yadav");
 	}
 }
