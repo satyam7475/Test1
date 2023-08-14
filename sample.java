d08f2e4c6ba14dfed4bfd482b87ad24dfb1c09f6
satyam yadav <satyamlost@gmail.com>
2023-08-14 13:20:47 +0530

    changes

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class sample{
     public static void main(String args[]){
         System.out.println("My name is satyam");
-        System.out.println("My name is satyam");
+        System.out.println("My name is ");
 
     }
 }
